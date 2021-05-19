551a563e38f9919bcfea73e4ae352dc522318e7b
bremxx <petr.kirienko@gmail.com>
2021-05-19 08:47:51 +0300

    добавлены статичные ресурсы и основные шаблоны

--- src/express/index.js
+++ src/express/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
@@ -10,6 +12,9 @@ const offersRoutes = require(`./routes/offers-routes`);
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`viewengine`, `pug`);
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
