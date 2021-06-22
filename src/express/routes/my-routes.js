a761762d18a97ff3f986e08fd1c72902f4c6e721
bremxx <petr.kirienko@gmail.com>
2021-06-23 00:17:41 +0300

    обработка запросов к странице с комментариями

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,6 +4,10 @@ const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
 const myRouter = new Router();
 
+myRouter.get(`/comments`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`comments`, {offers: offers.slice(0, 3)});
+});
 
 myRouter.get(`/`, async (req, res) => {
   const offers = await api.getOffers();
