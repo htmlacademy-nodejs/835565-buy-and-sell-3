f0e1fe415a4090b08a7ac05f12c5b04792bf62ac
bremxx <petr.kirienko@gmail.com>
2021-06-23 00:16:19 +0300

    обработка запроса к странице пользовательских объявлений

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const myRouter = new Router();
 
-myRouter.get(`/comments`, (request, response) => response.render(`comments`));
-myRouter.get(`/`, (request, response) => response.render(`my-tickets`));
+
+myRouter.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`my-tickets`, {offers});
+});
 
 module.exports = myRouter;
