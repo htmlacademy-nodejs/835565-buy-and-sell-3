33499d5eb294ea84498024bcfa6b85d018d80a78
bremxx <petr.kirienko@gmail.com>
2021-06-23 00:14:57 +0300

    обработка запроса к главной странице

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (request, response) => response.render(`main`));
-mainRouter.get(`/register`, (request, response) => response.render(`sign-up`));
-mainRouter.get(`/login`, (request, response) => response.render(`login`));
-mainRouter.get(`/search`, (request, response) => response.render(`search-result`));
+mainRouter.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`main`, {offers});
+});
+
 
 module.exports = mainRouter;
