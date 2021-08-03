c7e1f3ae790634196740f564814c442dc283e91f
bremxx <petr.kirienko@gmail.com>
2021-08-03 22:19:40 +0300

    функционал генерации сценария для заполнения БД моковыми данными

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -4,12 +4,14 @@ const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 const server = require(`./server`);
+const fill = require(`./fill`);
 
 const MocksGenerator = {
   [generate.name]: generate,
   [version.name]: version,
   [help.name]: help,
   [server.name]: server,
+  [fill.name]: fill,
 };
 
 module.exports = {MocksGenerator};
