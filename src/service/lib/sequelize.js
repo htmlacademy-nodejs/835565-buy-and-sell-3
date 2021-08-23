f293847e9432521daffa5cdd76af283a2d2fabda
bremxx <petr.kirienko@gmail.com>
2021-08-23 21:18:18 +0300

    установка соединения с СУБД

---
+++ src/service/lib/sequelize.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Sequelize = require(`sequelize`);
+const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
+
+const envVariablesAreMissing = [
+  DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+].some((variable) => variable === undefined);
+
+if (envVariablesAreMissing) {
+  throw new Error(`Some environmental variables are not defined`);
+}
+
+module.exports = new Sequelize(
+    DB_NAME, DB_USER, DB_PASSWORD, {
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: `postgres`,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 10000,
+        idle: 10000
+      }
+    }
+);
