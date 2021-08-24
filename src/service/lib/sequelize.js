61fbdaeb886ac19dedfb9adcedaa5c5e1a2b0c11
bremxx <petr.kirienko@gmail.com>
2021-08-24 19:34:59 +0300

    доп проверка необходимости наличия переменных окружения при вызове сценария запуска приложения

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -7,8 +7,10 @@ const envVariablesAreMissing = [
   DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 ].some((variable) => variable === undefined);
 
-if (envVariablesAreMissing && process.env.npm_lifecycle_event.includes(`start`)) {
-  throw new Error(`Some environmental variables are not defined`);
+if (process.env.npm_lifecycle_event) {
+  if (process.env.npm_lifecycle_event.includes(`start`) && envVariablesAreMissing) {
+    throw new Error(`Some environmental variables are not defined`);
+  }
 }
 
 module.exports = new Sequelize(
